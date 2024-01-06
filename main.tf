1113ab76c88a9714c54025744e19c9be6e522690
Rafael Lindoso <rafael.lindoso@gmail.com>
2024-01-06 00:46:29 -0300

    Create VM

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("key.json")
+
+  project = "gcpalura-410403"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "firstvm" {
+  name         = "firstvm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+}
